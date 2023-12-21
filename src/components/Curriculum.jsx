08485b33da04f9ddd6e98c63de884390d172f1d5
Eric9516 <eric-995-@hotmail.com>
2023-12-21 10:21:27 -0300

    completando curriculum

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import foto from "../../public/foto.jpg";
 import "../styles/curriculum.css";
+import { IoLogoCss3, IoLogoSass, IoLogoHtml5 } from "react-icons/io5";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaReact, FaBootstrap } from "react-icons/fa";
+import { GrMysql } from "react-icons/gr";
+import { SiFirebase, SiAdobephotoshop } from "react-icons/si";
+import { FaNode } from "react-icons/fa6";
+import { TbSql } from "react-icons/tb";
+import { Tooltip } from "react-tooltip";
 
 export const Curriculum = () => {
 	return (
@@ -45,7 +53,66 @@ export const Curriculum = () => {
 				</ul>
 			</div>
 			<div className="skills">
-				
+				<h3>SKILLS</h3>
+				<div className="tecnologias">
+					<IoLogoHtml5
+						data-tooltip-id="my-tooltip"
+						data-tooltip-content="HTML5"
+						data-tooltip-place="top"
+					/>
+					<IoLogoCss3
+						data-tooltip-id="my-tooltip"
+						data-tooltip-content="CSS3"
+						data-tooltip-place="top"
+					/>
+					<IoLogoJavascript
+						data-tooltip-id="my-tooltip"
+						data-tooltip-content="Javascript"
+						data-tooltip-place="top"
+					/>
+					<FaReact
+						data-tooltip-id="my-tooltip"
+						data-tooltip-content="React.js"
+						data-tooltip-place="top"
+					/>
+					<FaBootstrap
+						data-tooltip-id="my-tooltip"
+						data-tooltip-content="Bootstrap"
+						data-tooltip-place="top"
+					/>
+					<GrMysql
+						data-tooltip-id="my-tooltip"
+						data-tooltip-content="MySQL"
+						data-tooltip-place="top"
+					/>
+					<TbSql
+						data-tooltip-id="my-tooltip"
+						data-tooltip-content="SQL"
+						data-tooltip-place="top"
+					/>
+					<SiFirebase
+						data-tooltip-id="my-tooltip"
+						data-tooltip-content="Firebase"
+						data-tooltip-place="top"
+					/>
+					<SiAdobephotoshop
+						data-tooltip-id="my-tooltip"
+						data-tooltip-content="Adobe Photoshop"
+						data-tooltip-place="top"
+					/>
+					<FaNode
+						data-tooltip-id="my-tooltip"
+						data-tooltip-content="Node.Js"
+						data-tooltip-place="top"
+					/>
+					<IoLogoSass
+						data-tooltip-id="my-tooltip"
+						data-tooltip-content="Sass"
+						data-tooltip-place="top"
+					/>
+				</div>
+				{/* Componente ReactTooltip */}
+				<Tooltip id="my-tooltip" />
 			</div>
 		</div>
 	);
